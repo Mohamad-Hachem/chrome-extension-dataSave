b246f0622c7b720731a054a747d3de2929c7e7a2
Hachem <mohamad27hachem@outlook.com>
2021-12-05 18:11:30 +0100

    try

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 const tabBtn = document.getElementById("tab-btn")
 const copyBtn = document.getElementById("copy-btn")
 const extractBtn = document.getElementById("Extract-btn")
-const textarea = document.getElementById("textarea-el")
+const copiedEl = document.getElementById("copied-el")
 
+console.log("this is the 2 btns"+copyBtn.textContent +" "+extractBtn.textContent)
 if (leadsFromLocalStorage) 
 {
     myLeads = leadsFromLocalStorage
@@ -29,9 +31,13 @@ function render(leads)
     let listItems = ""
     for (let i = 0; i < leads.length; i++) 
     {
-        listItems += leads[i]
+        listItems +=`
+            <li>
+                    ${leads[i]}
+            </li>
+        `
     }
-    textarea.innerHTML = listItems
+    ulEl.innerHTML = listItems
 }
 
 deleteBtn.addEventListener("dblclick", function() 
@@ -43,68 +49,35 @@ deleteBtn.addEventListener("dblclick", function()
 
 inputBtn.addEventListener("click", function() 
 {
-    //$$$$$$$$$$$$$
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
-    //$$$$$$$$$$$$$$
-    // chrome.tabs.executeScript( 
-    //     {
-    //       code: "window.getSelection().toString();"
-    //     }, function(selection) 
-    //     {
-    //       textarea.value = selection[0];
-    //     }
-    // );
 })
 
-    // chrome.extension.onRequest.addListener(function(request,sender,sendResponse)
-    // {
-    //     if(request.method == "getSelection")
-    //     {
-    //         //let selection = getSelectionHTML()
-    //         sendResponse(
-    //         {
-    //             data: selection, url:window.location.href, subject :document.title
-    //         })
-    //         myLeads.push(selection)
-    //         localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    //         render(myLeads)
-    //     }
-    //     else
-    //         sendResponse({})    
-    // })
-//})
+copyBtn.addEventListener("click",function()
+{
+    let string = ""
+    for(let i = 0 ;i<myLeads.length;i++)
+    {
+        string += myLeads[i] + "\n"
+    }
+    navigator.clipboard.writeText(string)
+    copiedEl.textContent = "Copied"
+    //alert("copied")
+})
 
-//Gets the html od the user's selection
-// function getSelectionHTML()
-// {
-//     let userSelection ;
-//     if(window.getSelection)
-//     {
-//         //W3C ranges
-//         userSelection = window.getSelection();
-//         if(userSelection.getRangeAt)
-//             let range = userSelection.getRangeAt(0)
-//         else
-//         {
-//             let range = document.createRange()
-//             range.setStart(userSelection.anchorNode, userSelection.anchorOffset)
-//             range.setEnd(userSelection.focusNode, userSelection.focusOffset)
-//         }
-//         let clonedSelection = range.cloneContents()
-//         let div = document.createElement('div')
-//         div.appendChild(clonedSelection)
-//         return div.innerHTML
-//     }
-//     else if(document.selection)
-//     {
-//         userSelection = document.selection.createRange()
-//         return userSelection.htmlText
-//     }
-//     else
-//     {
-//         return ''
-//     }
-// }
+extractBtn.addEventListener("click", function()
+{
+    let string = ""
+    for(let i = 0 ; i < myLeads.length ; i++)
+    {
+        string += myLeads[i] + "\n"
+    }
+    //let FileSaver = require('file-saver')
+    //let blob = new blob(["string"],{type: 'text/plain;charset=utf-8"'})
+    require FileSaver.js
+    FileSaver.saveAs(["string"],{type: 'text/plain;charset=utf-8"'},'ChromExtension.txt')
+    copiedEl.textContent = "extarcted"
+})
+
